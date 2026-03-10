7da7ce67ddfa53d9c788668b5922f77a447f6d41
sakuu1915 <sakshihavaldar221@gmail.com>
2026-03-11 00:04:20 +0530

    Fix package.json for Render deploy

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -49,7 +49,7 @@ function Footer() {
     </div>
   </nav>
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © {new Date().getFullYear()} - Sakshi Havaldar</p>
   </aside>
 </footer>
     </div>
